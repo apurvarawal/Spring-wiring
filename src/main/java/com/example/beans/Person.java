1e9cafdcc7f83b10a87f83fd2f8b4f34a61476f7
Apurva <apurva2928@gmail.com>
2022-12-29 22:32:50 +0530

    first changes

---
+++ src/main/java/com/example/beans/Person.java
@@ -0,0 +1,18 @@
+package com.example.beans;
+
+public class Person {
+    public String name;
+    public Vehicle vehicle;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Vehicle getVehicle(){
+        return vehicle;
+    }
+}
